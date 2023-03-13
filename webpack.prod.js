228ee5ffa6da1e835730601524ea5380fc7f0cb6
Alexey Gamov <lexa2009xxxx@yandex.ru>
2023-03-13 16:47:35 +0300

    wp template installed

--- webpack.prod.js
+++ webpack.prod.js
@@ -7,7 +7,7 @@ const ImageMinimizerPlugin = require("image-minimizer-webpack-plugin");
 const TerserPlugin = require("terser-webpack-plugin");
 const { merge } = require("webpack-merge");
 const common = require("./webpack.common");
-const PAGES = require("./entries.config");
+const PAGES = require("./wentries.config");
 
 module.exports = merge(common, {
   mode: "production",
