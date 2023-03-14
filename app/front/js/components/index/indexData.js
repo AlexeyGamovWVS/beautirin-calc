83b3e9e0e65ef658dc0c6a11ec47419db21b896b
Alexey Gamov <lexa2009xxxx@yandex.ru>
2023-03-14 14:41:37 +0300

    fix: refactor 1/2 (видимые изменения интерфейса, без логики

---
+++ app/front/js/components/index/indexData.js
@@ -0,0 +1,38 @@
+// PAGE ELEMETS START
+// eslint-disable-next-line no-undef
+const CALC = document.querySelector(".btr-form");
+// eslint-disable-next-line no-undef
+const IMG = document.querySelector(".btr-imagebox__image");
+
+const COLOR_RADIOS = Array.from(
+  CALC.querySelectorAll('input[data-t="field:input-color"]')
+);
+console.log(COLOR_RADIOS);
+const SIZE_SELECT = CALC.querySelector("#size");
+const AMOUNT_RADIOS = Array.from(
+  CALC.querySelectorAll('input[data-t="field:input-amount"]')
+);
+const AMOUNT_CASSETS = CALC.querySelector(
+  'input[data-t="field:input-cassetsamount"]'
+);
+const AMOUNT_CAPTION = CALC.querySelector("#cassetsamount_caption");
+const SUBMIT = CALC.querySelector(".btr-form__submit");
+const PRICE = CALC.querySelector(".btr-form__price");
+const UPAKOVKA = CALC.querySelector("input[data-t='field:input-proizvodstvo']");
+
+// PAGE ELEMENTS END
+
+const BTR_FORM_FIELDS = {
+  CALC,
+  IMG,
+  COLOR_RADIOS,
+  SIZE_SELECT,
+  AMOUNT_CAPTION,
+  AMOUNT_CASSETS,
+  AMOUNT_RADIOS,
+  SUBMIT,
+  PRICE,
+  UPAKOVKA,
+};
+
+export default BTR_FORM_FIELDS;
--- app/front/js/pages/index.js
+++ app/front/js/pages/index.js
@@ -1,245 +1,121 @@
+/* eslint-disable no-use-before-define */
+/* eslint-disable no-unused-vars */
+/* eslint-disable no-param-reassign */
 import "../utils/utils";
 import "../../scss/pages/index.scss";
 import IMAGES_SOURCES from "../utils/images-data";
 import PRICES from "../utils/prices-data";
 import numberWithSpaces from "../components/index/numberspaces";
-// PAGE ELEMETS START
-// eslint-disable-next-line no-undef
-const CALC = document.querySelector(".btr-form");
-// eslint-disable-next-line no-undef
-const IMG = document.querySelector(".btr-imagebox__image");
-
-const COLOR_RADIOS = Array.from(
-  CALC.querySelectorAll('input[data-t="field:input-color"]')
-);
-const SIZE_SELECT = CALC.querySelector("#size");
-const AMOUNT_RADIOS = Array.from(
-  CALC.querySelectorAll('input[data-t="field:input-amount"]')
-);
-const AMOUNT_CASSETS = CALC.querySelector(
-  'input[data-t="field:input-cassetsamount"]'
-);
-const AMOUNT_CAPTION = CALC.querySelector("#cassetsamount_caption");
-const SUBMIT = CALC.querySelector(".btr-form__submit");
-const PRICE = CALC.querySelector(".btr-form__price");
-const UPAKOVKA = CALC.querySelector("input[data-t='field:input-proizvodstvo']");
-
-// PAGE ELEMENTS END
+import BTR_FORM_FIELDS from "../components/index/indexData";
 
 // INITIAL STATE START
-const initialColor = COLOR_RADIOS.find((radio) => radio.checked).value;
-const initialSize = SIZE_SELECT.value;
 
 const CALC_DATA = {
-  color: initialColor,
-  size: initialSize,
-  imgName: initialColor[0] + initialSize,
-  imgSrc: IMAGES_SOURCES[`${initialColor[0]}${initialSize}`],
+  color: "",
+  size: 0,
+  prevSize: 0,
+  imgName: "",
+  imgSrc: "",
+  prevImgSrc: "",
   casset: 0,
-  casset_amount: 0,
+  cassetAmount: 0,
   totalAmount: 0,
-  price: 4,
+  price: 0,
   packPrice: 0,
   totalPackPrice: 0,
   packing: "Без упаковки",
   totalPrice: 0,
 };
 
-IMG.src = CALC_DATA.imgSrc;
-
-function changeImageByParams(colorId, sizeId) {
-  let { color, size, imgSrc } = CALC_DATA;
-  color = colorId;
-  size = sizeId;
-  imgSrc = IMAGES_SOURCES[`${color[0]}${size}`];
-  // eslint-disable-next-line no-unused-vars
+function changeImageByParams({ imgSrc }, { IMG }) {
   IMG.src = imgSrc;
 }
-function canselDisabledAmount() {
+function setInitialState(data, form, images) {
+  data.color = form.COLOR_RADIOS.find((radio) => radio.checked).value;
+  data.size = +form.SIZE_SELECT.value;
+  data.prevSize = +form.SIZE_SELECT.value;
+  data.imgName = data.color[0] + data.size;
+  data.imgSrc = images[data.imgName];
+  data.prevImgSrc = data.imgSrc;
+
+  changeImageByParams(data, form);
+  setEventListeners(data, form, images);
+}
+
+function setEventListeners(data, form, images) {
+  setColorListener(data, images, form);
+  setSizeListener(data, images, form);
+  setCassetSizeListener(data, form);
+  setAmountCassetListener(data, form);
+  setAdditionalServListener(data, form);
+  setSubmitListener(data, form);
+}
+
+function setColorListener(data, images, { COLOR_RADIOS, ...form }) {
+  COLOR_RADIOS.forEach((radio) => {
+    radio.addEventListener("change", () => {
+      data.color = radio.value;
+      data.imgName = data.color[0] + data.size;
+      data.imgSrc = images[data.imgName];
+      changeImageByParams(data, form);
+    });
+  });
+}
+
+function setSizeListener(data, images, { SIZE_SELECT, ...form }) {
+  SIZE_SELECT.addEventListener("change", () => {
+    if (SIZE_SELECT.value) {
+      const sizeNname = +SIZE_SELECT.value === 2.5 ? "2p5" : SIZE_SELECT.value;
+      data.size = +SIZE_SELECT.value;
+      data.imgName = data.color[0] + sizeNname;
+      data.imgSrc = images[data.imgName];
+      changeImageByParams(data, form);
+    }
+  });
+}
+
+function setTotalAmount(data) {
+  data.totalAmount = data.cassetAmount * data.casset;
+}
+
+function changeAmountCaption(
+  { cassetAmount, casset, totalAmount },
+  { AMOUNT_CAPTION }
+) {
+  AMOUNT_CAPTION.textContent = `${cassetAmount} кассет * ${casset} шт в кассете = ${totalAmount} ампул`;
+}
+
+function setCassetSizeListener(data, { AMOUNT_RADIOS, ...form }) {
   AMOUNT_RADIOS.forEach((radio) => {
-    radio.removeAttribute("disabled");
-    // eslint-disable-next-line no-param-reassign
-    radio.parentNode.nextSibling.style.opacity = 1;
+    radio.addEventListener("change", () => {
+      data.casset = +radio.value;
+      setTotalAmount(data);
+      changeAmountCaption(data, form);
+    });
   });
 }
-function printPrice() {
-  PRICE.textContent = `${numberWithSpaces(CALC_DATA.totalPrice)} ₽`;
-}
-function updatePackPrice() {
-  CALC_DATA.packPrice = +CALC_DATA.casset * CALC_DATA.price;
-  CALC_DATA.totalPackPrice = CALC_DATA.packPrice * CALC_DATA.casset_amount;
-}
-function updatePrice() {
-  updatePackPrice();
-  const upac =
-    CALC_DATA.packing === "Без упаковки" ? 0 : CALC_DATA.totalPackPrice;
-  CALC_DATA.totalPrice = CALC_DATA.totalAmount * CALC_DATA.price + upac;
-  printPrice();
-}
-function checkInvalidRadio(radio) {
-  if (radio.checked) {
-    CALC_DATA.price = 0;
-    updatePrice();
-    // eslint-disable-next-line no-param-reassign
-    radio.checked = false;
-  }
-}
-function setDisabledAmount() {
-  canselDisabledAmount();
-  let filterArr = [];
-  let newArr = [];
-  const { size } = CALC_DATA;
-  switch (size) {
-    case "2":
-      filterArr = ["amount2", "amount3"];
-      newArr = AMOUNT_RADIOS.filter(
-        (radio) => filterArr.indexOf(radio.id) >= 0
-      );
-      newArr.forEach((radio) => {
-        radio.setAttribute("disabled", true);
-        checkInvalidRadio(radio);
-        // eslint-disable-next-line no-param-reassign
-        radio.parentNode.nextSibling.style.opacity = 0.3;
-      });
-      break;
-    case "2p5":
-      filterArr = ["amount1", "amount2", "amount3"];
-      newArr = AMOUNT_RADIOS.filter(
-        (radio) => filterArr.indexOf(radio.id) >= 0
-      );
-      newArr.forEach((radio) => {
-        radio.setAttribute("disabled", true);
-        checkInvalidRadio(radio);
-        // eslint-disable-next-line no-param-reassign
-        radio.parentNode.nextSibling.style.opacity = 0.3;
-      });
-      break;
-    case "4":
-      filterArr = ["amount2", "amount3"];
-      newArr = AMOUNT_RADIOS.filter(
-        (radio) => filterArr.indexOf(radio.id) >= 0
-      );
-      newArr.forEach((radio) => {
-        radio.setAttribute("disabled", true);
-        checkInvalidRadio(radio);
-        // eslint-disable-next-line no-param-reassign
-        radio.parentNode.nextSibling.style.opacity = 0.3;
-      });
-      break;
-    case "5":
-      filterArr = ["amount1", "amount2", "amount3"];
-      newArr = AMOUNT_RADIOS.filter(
-        (radio) => filterArr.indexOf(radio.id) >= 0
-      );
-      newArr.forEach((radio) => {
-        radio.setAttribute("disabled", true);
-        checkInvalidRadio(radio);
-        // eslint-disable-next-line no-param-reassign
-        radio.parentNode.nextSibling.style.opacity = 0.3;
-      });
-      break;
-    case "10":
-      filterArr = ["amount1", "amount2", "amount5"];
-      newArr = AMOUNT_RADIOS.filter(
-        (radio) => filterArr.indexOf(radio.id) >= 0
-      );
-      newArr.forEach((radio) => {
-        radio.setAttribute("disabled", true);
-        checkInvalidRadio(radio);
-        // eslint-disable-next-line no-param-reassign
-        radio.parentNode.nextSibling.style.opacity = 0.3;
-      });
-      break;
-    case "15":
-      filterArr = ["amount1", "amount2", "amount5"];
-      newArr = AMOUNT_RADIOS.filter(
-        (radio) => filterArr.indexOf(radio.id) >= 0
-      );
-      newArr.forEach((radio) => {
-        radio.setAttribute("disabled", true);
-        checkInvalidRadio(radio);
-        // eslint-disable-next-line no-param-reassign
-        radio.parentNode.nextSibling.style.opacity = 0.3;
-      });
-      break;
-    case "20":
-      filterArr = ["amount1", "amount2", "amount3"];
-      newArr = AMOUNT_RADIOS.filter(
-        (radio) => filterArr.indexOf(radio.id) >= 0
-      );
-      newArr.forEach((radio) => {
-        radio.setAttribute("disabled", true);
-        checkInvalidRadio(radio);
-        // eslint-disable-next-line no-param-reassign
-        radio.parentNode.nextSibling.style.opacity = 0.3;
-      });
-      break;
-    case "50":
-      filterArr = ["amount3", "amount5"];
-      newArr = AMOUNT_RADIOS.filter(
-        (radio) => filterArr.indexOf(radio.id) >= 0
-      );
-      newArr.forEach((radio) => {
-        radio.setAttribute("disabled", true);
-        checkInvalidRadio(radio);
-        // eslint-disable-next-line no-param-reassign
-        radio.parentNode.nextSibling.style.opacity = 0.3;
-      });
-      break;
-    default:
-      break;
-  }
-}
-function changeAmountCaption() {
-  CALC_DATA.totalAmount = +CALC_DATA.casset_amount * +CALC_DATA.casset;
-  AMOUNT_CAPTION.textContent = `${CALC_DATA.casset_amount} кассет * ${CALC_DATA.casset} шт в кассете = ${CALC_DATA.totalAmount} ампул`;
-}
 
-changeImageByParams(CALC_DATA.color[0], CALC_DATA.size);
-setDisabledAmount();
-changeAmountCaption();
-updatePrice();
-// INITIAL STATE END
-
-// ADD LISTENERS
-
-SIZE_SELECT.addEventListener("change", () => {
-  CALC_DATA.size = SIZE_SELECT.value === "2,5" ? "2p5" : SIZE_SELECT.value;
-  setDisabledAmount();
-  changeImageByParams(CALC_DATA.color, CALC_DATA.size);
-  CALC_DATA.price = PRICES[`${CALC_DATA.color[0]}${CALC_DATA.size}`];
-  updatePrice();
-});
-
-COLOR_RADIOS.forEach((radio) => {
-  radio.addEventListener("change", () => {
-    CALC_DATA.color = radio.value;
-    changeImageByParams(CALC_DATA.color, CALC_DATA.size);
+function setAmountCassetListener(data, { AMOUNT_CASSETS, ...form }) {
+  AMOUNT_CASSETS.addEventListener("input", () => {
+    data.cassetAmount = +AMOUNT_CASSETS.value;
+    setTotalAmount(data);
+    changeAmountCaption(data, form);
   });
-});
+}
 
-AMOUNT_RADIOS.forEach((radio) => {
-  radio.addEventListener("change", () => {
-    CALC_DATA.casset = radio.value;
-    changeAmountCaption();
-    updatePrice();
+function setAdditionalServListener(data, { UPAKOVKA }) {
+  UPAKOVKA.addEventListener("change", () => {
+    data.packing = UPAKOVKA.checked
+      ? "Упаковать в товарный вид"
+      : "Без упаковки";
   });
-});
+}
 
-AMOUNT_CASSETS.addEventListener("input", () => {
-  CALC_DATA.casset_amount = AMOUNT_CASSETS.value;
-  changeAmountCaption();
-  updatePrice();
-});
+function setSubmitListener(data, { SUBMIT }) {
+  SUBMIT.addEventListener("click", (e) => {
+    e.preventDefault();
+    console.log(data);
+  });
+}
 
-UPAKOVKA.addEventListener("change", () => {
-  CALC_DATA.packing = UPAKOVKA.checked
-    ? "Упаковать в товарный вид"
-    : "Без упаковки";
-  updatePrice();
-});
-
-SUBMIT.addEventListener("click", (e) => {
-  e.preventDefault();
-  console.log(CALC_DATA);
-});
+setInitialState(CALC_DATA, BTR_FORM_FIELDS, IMAGES_SOURCES);
