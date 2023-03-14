fdaee74b8adda72007a6346de13ec6b92b1a8beb
Alexey Gamov <lexa2009xxxx@yandex.ru>
2023-03-14 22:32:29 +0300

    feat: подключить фотки

--- app/front/js/pages/index/images-data.js
+++ app/front/js/pages/index/images-data.js
@@ -1,53 +1,53 @@
-import w2 from "../../../../images/2w.png";
-import b2 from "../../../../images/2b.png";
-import t2 from "../../../../images/2t.png";
-import w2p5 from "../../../../images/2p5w.png";
-import b2p5 from "../../../../images/2p5b.png";
-import t2p5 from "../../../../images/2p5t.png";
-import w4 from "../../../../images/4w.png";
-import b4 from "../../../../images/4b.png";
-import t4 from "../../../../images/4t.png";
-import w5 from "../../../../images/5w.png";
-import b5 from "../../../../images/5b.png";
-import t5 from "../../../../images/5t.png";
-import w10 from "../../../../images/10w.png";
-import b10 from "../../../../images/10b.png";
-import t10 from "../../../../images/10t.png";
-import w15 from "../../../../images/15w.png";
-import b15 from "../../../../images/15b.png";
-import t15 from "../../../../images/15t.png";
-import w20 from "../../../../images/20w.png";
-import b20 from "../../../../images/20b.png";
-import t20 from "../../../../images/20t.png";
-import w50 from "../../../../images/50w.png";
-import b50 from "../../../../images/50b.png";
-import t50 from "../../../../images/50t.png";
+// import w2 from "../../../../images/2w.png";
+// import b2 from "../../../../images/2b.png";
+// import t2 from "../../../../images/2t.png";
+// import w2p5 from "../../../../images/2p5w.png";
+// import b2p5 from "../../../../images/2p5b.png";
+// import t2p5 from "../../../../images/2p5t.png";
+// import w4 from "../../../../images/4w.png";
+// import b4 from "../../../../images/4b.png";
+// import t4 from "../../../../images/4t.png";
+// import w5 from "../../../../images/5w.png";
+// import b5 from "../../../../images/5b.png";
+// import t5 from "../../../../images/5t.png";
+// import w10 from "../../../../images/10w.png";
+// import b10 from "../../../../images/10b.png";
+// import t10 from "../../../../images/10t.png";
+// import w15 from "../../../../images/15w.png";
+// import b15 from "../../../../images/15b.png";
+// import t15 from "../../../../images/15t.png";
+// import w20 from "../../../../images/20w.png";
+// import b20 from "../../../../images/20b.png";
+// import t20 from "../../../../images/20t.png";
+// import w50 from "../../../../images/50w.png";
+// import b50 from "../../../../images/50b.png";
+// import t50 from "../../../../images/50t.png";
 
 const IMAGES_SOURCES = {
-  w2,
-  b2,
-  t2,
-  w2p5,
-  b2p5,
-  t2p5,
-  w4,
-  b4,
-  t4,
-  w5,
-  b5,
-  t5,
-  w10,
-  b10,
-  t10,
-  w15,
-  b15,
-  t15,
-  w20,
-  b20,
-  t20,
-  w50,
-  b50,
-  t50,
+  w2: "https://static.tildacdn.com/tild6562-3336-4762-b938-396434376631/2w.png",
+  b2: "https://static.tildacdn.com/tild3530-3466-4439-b336-366364633138/2b.png",
+  t2: "https://static.tildacdn.com/tild3366-6464-4839-b133-386235333130/2t.png",
+  w2p5: "https://static.tildacdn.com/tild3563-6138-4135-b232-326662316665/2p5w.png",
+  b2p5: "https://static.tildacdn.com/tild3362-3237-4665-b835-366536386635/2p5b.png",
+  t2p5: "https://static.tildacdn.com/tild3165-3164-4133-a661-363832643030/2p5t.png",
+  w4: "https://static.tildacdn.com/tild3061-6534-4331-a265-616439313230/4w.png",
+  b4: "https://static.tildacdn.com/tild3531-3537-4830-a433-336164386562/4b.png",
+  t4: "https://static.tildacdn.com/tild3366-6231-4132-b735-333863373938/4t.png",
+  w5: "https://static.tildacdn.com/tild6431-6337-4464-b636-383632646465/5w.png",
+  b5: "https://static.tildacdn.com/tild6462-3633-4332-a432-373665396463/5b.png",
+  t5: "https://static.tildacdn.com/tild3933-3636-4338-b133-653434636433/5t.png",
+  b10: "https://static.tildacdn.com/tild6432-6461-4433-a139-306566376366/10b.png",
+  t10: "https://static.tildacdn.com/tild3231-6336-4166-a233-623437356434/10t.png",
+  w10: "https://static.tildacdn.com/tild6132-3635-4661-a532-333163386534/10w.png",
+  w15: "https://static.tildacdn.com/tild6362-6564-4237-b162-303034633536/15w.png",
+  b15: "https://static.tildacdn.com/tild3531-3933-4537-a536-633263366661/15b.png",
+  t15: "https://static.tildacdn.com/tild6662-3731-4263-a139-383463343033/15t.png",
+  w20: "https://static.tildacdn.com/tild3966-6162-4637-a363-663839343335/20w.png",
+  b20: "https://static.tildacdn.com/tild3738-6631-4435-b832-303364623266/20b.png",
+  t20: "https://static.tildacdn.com/tild3961-6664-4364-b539-663062616239/20t.png",
+  w50: "https://static.tildacdn.com/tild6135-3533-4637-b766-633638613731/50w.png",
+  b50: "https://static.tildacdn.com/tild3364-3561-4037-b536-663066386233/50b.png",
+  t50: "https://static.tildacdn.com/tild3936-6461-4463-a462-386535313431/50t.png",
 };
 
 export default IMAGES_SOURCES;
