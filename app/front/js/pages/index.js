42c707fd80137ee25d98f60e9881b6d365bbca36
Alexey Gamov <lexa2009xxxx@yandex.ru>
2023-03-14 03:31:17 +0300

    feat: настроить расчёт в калькуляторе

--- app/front/js/pages/index.js
+++ app/front/js/pages/index.js
@@ -1,7 +1,8 @@
 import "../utils/utils";
 import "../../scss/pages/index.scss";
 import IMAGES_SOURCES from "../utils/images-data";
-
+import PRICES from "../utils/prices-data";
+import numberWithSpaces from "../components/index/numberspaces";
 // PAGE ELEMETS START
 // eslint-disable-next-line no-undef
 const CALC = document.querySelector(".btr-form");
@@ -15,7 +16,13 @@ const SIZE_SELECT = CALC.querySelector("#size");
 const AMOUNT_RADIOS = Array.from(
   CALC.querySelectorAll('input[data-t="field:input-amount"]')
 );
+const AMOUNT_CASSETS = CALC.querySelector(
+  'input[data-t="field:input-cassetsamount"]'
+);
+const AMOUNT_CAPTION = CALC.querySelector("#cassetsamount_caption");
 const SUBMIT = CALC.querySelector(".btr-form__submit");
+const PRICE = CALC.querySelector(".btr-form__price");
+const UPAKOVKA = CALC.querySelector("input[data-t='field:input-proizvodstvo']");
 
 // PAGE ELEMENTS END
 
@@ -29,17 +36,23 @@ const CALC_DATA = {
   imgName: initialColor[0] + initialSize,
   imgSrc: IMAGES_SOURCES[`${initialColor[0]}${initialSize}`],
   casset: 0,
+  casset_amount: 0,
+  totalAmount: 0,
+  price: 4,
+  packPrice: 0,
+  totalPackPrice: 0,
+  packing: "Без упаковки",
+  totalPrice: 0,
 };
 
 IMG.src = CALC_DATA.imgSrc;
 
 function changeImageByParams(colorId, sizeId) {
-  let { color, size, imgSrc, imgName } = CALC_DATA;
+  let { color, size, imgSrc } = CALC_DATA;
   color = colorId;
   size = sizeId;
   imgSrc = IMAGES_SOURCES[`${color[0]}${size}`];
   // eslint-disable-next-line no-unused-vars
-  imgName = `${color[0]}${size}`;
   IMG.src = imgSrc;
 }
 function canselDisabledAmount() {
@@ -48,6 +61,28 @@ function canselDisabledAmount() {
     // eslint-disable-next-line no-param-reassign
     radio.parentNode.nextSibling.style.opacity = 1;
   });
+}
+function printPrice() {
+  PRICE.textContent = `${numberWithSpaces(CALC_DATA.totalPrice)} ₽`;
+}
+function updatePackPrice() {
+  CALC_DATA.packPrice = +CALC_DATA.casset * CALC_DATA.price;
+  CALC_DATA.totalPackPrice = CALC_DATA.packPrice * CALC_DATA.casset_amount;
+}
+function updatePrice() {
+  updatePackPrice();
+  const upac =
+    CALC_DATA.packing === "Без упаковки" ? 0 : CALC_DATA.totalPackPrice;
+  CALC_DATA.totalPrice = CALC_DATA.totalAmount * CALC_DATA.price + upac;
+  printPrice();
+}
+function checkInvalidRadio(radio) {
+  if (radio.checked) {
+    CALC_DATA.price = 0;
+    updatePrice();
+    // eslint-disable-next-line no-param-reassign
+    radio.checked = false;
+  }
 }
 function setDisabledAmount() {
   canselDisabledAmount();
@@ -62,17 +97,19 @@ function setDisabledAmount() {
       );
       newArr.forEach((radio) => {
         radio.setAttribute("disabled", true);
+        checkInvalidRadio(radio);
         // eslint-disable-next-line no-param-reassign
         radio.parentNode.nextSibling.style.opacity = 0.3;
       });
       break;
-    case "2,5":
+    case "2p5":
       filterArr = ["amount1", "amount2", "amount3"];
       newArr = AMOUNT_RADIOS.filter(
         (radio) => filterArr.indexOf(radio.id) >= 0
       );
       newArr.forEach((radio) => {
         radio.setAttribute("disabled", true);
+        checkInvalidRadio(radio);
         // eslint-disable-next-line no-param-reassign
         radio.parentNode.nextSibling.style.opacity = 0.3;
       });
@@ -84,6 +121,7 @@ function setDisabledAmount() {
       );
       newArr.forEach((radio) => {
         radio.setAttribute("disabled", true);
+        checkInvalidRadio(radio);
         // eslint-disable-next-line no-param-reassign
         radio.parentNode.nextSibling.style.opacity = 0.3;
       });
@@ -95,6 +133,7 @@ function setDisabledAmount() {
       );
       newArr.forEach((radio) => {
         radio.setAttribute("disabled", true);
+        checkInvalidRadio(radio);
         // eslint-disable-next-line no-param-reassign
         radio.parentNode.nextSibling.style.opacity = 0.3;
       });
@@ -106,6 +145,7 @@ function setDisabledAmount() {
       );
       newArr.forEach((radio) => {
         radio.setAttribute("disabled", true);
+        checkInvalidRadio(radio);
         // eslint-disable-next-line no-param-reassign
         radio.parentNode.nextSibling.style.opacity = 0.3;
       });
@@ -117,6 +157,7 @@ function setDisabledAmount() {
       );
       newArr.forEach((radio) => {
         radio.setAttribute("disabled", true);
+        checkInvalidRadio(radio);
         // eslint-disable-next-line no-param-reassign
         radio.parentNode.nextSibling.style.opacity = 0.3;
       });
@@ -128,6 +169,7 @@ function setDisabledAmount() {
       );
       newArr.forEach((radio) => {
         radio.setAttribute("disabled", true);
+        checkInvalidRadio(radio);
         // eslint-disable-next-line no-param-reassign
         radio.parentNode.nextSibling.style.opacity = 0.3;
       });
@@ -139,6 +181,7 @@ function setDisabledAmount() {
       );
       newArr.forEach((radio) => {
         radio.setAttribute("disabled", true);
+        checkInvalidRadio(radio);
         // eslint-disable-next-line no-param-reassign
         radio.parentNode.nextSibling.style.opacity = 0.3;
       });
@@ -147,16 +190,25 @@ function setDisabledAmount() {
       break;
   }
 }
+function changeAmountCaption() {
+  CALC_DATA.totalAmount = +CALC_DATA.casset_amount * +CALC_DATA.casset;
+  AMOUNT_CAPTION.textContent = `${CALC_DATA.casset_amount} кассет * ${CALC_DATA.casset} шт в кассете = ${CALC_DATA.totalAmount} ампул`;
+}
+
 changeImageByParams(CALC_DATA.color[0], CALC_DATA.size);
 setDisabledAmount();
+changeAmountCaption();
+updatePrice();
 // INITIAL STATE END
 
 // ADD LISTENERS
 
 SIZE_SELECT.addEventListener("change", () => {
-  CALC_DATA.size = SIZE_SELECT.value;
+  CALC_DATA.size = SIZE_SELECT.value === "2,5" ? "2p5" : SIZE_SELECT.value;
   setDisabledAmount();
   changeImageByParams(CALC_DATA.color, CALC_DATA.size);
+  CALC_DATA.price = PRICES[`${CALC_DATA.color[0]}${CALC_DATA.size}`];
+  updatePrice();
 });
 
 COLOR_RADIOS.forEach((radio) => {
@@ -169,14 +221,25 @@ COLOR_RADIOS.forEach((radio) => {
 AMOUNT_RADIOS.forEach((radio) => {
   radio.addEventListener("change", () => {
     CALC_DATA.casset = radio.value;
+    changeAmountCaption();
+    updatePrice();
   });
+});
+
+AMOUNT_CASSETS.addEventListener("input", () => {
+  CALC_DATA.casset_amount = AMOUNT_CASSETS.value;
+  changeAmountCaption();
+  updatePrice();
+});
+
+UPAKOVKA.addEventListener("change", () => {
+  CALC_DATA.packing = UPAKOVKA.checked
+    ? "Упаковать в товарный вид"
+    : "Без упаковки";
+  updatePrice();
 });
 
 SUBMIT.addEventListener("click", (e) => {
   e.preventDefault();
-  console.log(CALC_DATA.color);
-  console.log(CALC_DATA.size);
-  console.log(CALC_DATA.imgSrc);
-  console.log(CALC_DATA.imgName);
-  console.log(CALC_DATA.casset);
+  console.log(CALC_DATA);
 });
